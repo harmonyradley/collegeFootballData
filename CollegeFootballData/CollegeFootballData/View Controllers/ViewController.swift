f936b59af9e3ee16457da884269e530c6f64c30b
Harmony Radley <harmonyradley99@gmail.com>
2021-07-27 14:58:23 -0400

    Created Xcode project and folders for files

---
+++ CollegeFootballData/CollegeFootballData/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollegeFootballData
+//
+//  Created by Harmony Radley on 7/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
